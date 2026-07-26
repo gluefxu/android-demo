665a3f0c77c83908c28012d35d3a5cb7037a8465
gluefxu <gluefx@outlook.com>
2026-07-26 06:24:09 +0000

    feat: 统一配置版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     namespace = "com.demo.basicviewsactivity"
     compileSdk {
         version = release(36) {
-            minorApiLevel = 1
+            minorApiLevel = 0
         }
     }
 
